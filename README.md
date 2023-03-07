a896369a3a4de9106381913d63cc44b8ee8e2545
Mark-Randria <randrianjafymark2.0@gmail.com>
2023-03-07 13:36:01 +0300

    Rearranging messy folders

--- README.md
+++
@@ -1,3 +0,0 @@
-# devhunt2023
-
-DevHunt 2ème édition organisé par l'ENI Fianarantsoa , année 2023
--- middlewares/not-found.js
+++
@@ -1,3 +0,0 @@
-const notFound = (req,res) => res.status(404).json({ msg: 'No route matching' })
-
-module.exports = { notFound }
